2f25123ce904b582561fe3430b70a3117b0079ff
oneren37 <oneren37@gmail.com>
2021-06-29 14:52:01 +0300

    final

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-import numpy as np
 import pymysql
 from flask import Flask, request
 import json
@@ -8,8 +7,91 @@ app = Flask(__name__)
 
 @app.route('/users', methods=['POST'])
 def users():
-
     req = request.get_json(force=True)
+    insert_query = "INSERT INTO `user` (first_name, last_name, email, math, rus, phys, inf, individual) VALUES " \
+                   "('%s', '%s', '%s', %i, %i, %i, %i, %i);" % (
+                       req["firstname"], req["lastname"], req["email"],
+                       req["math"], req["rus"], req["phys"], req["inf"],
+                       req["individual"])
+
+    dbFetch(insert_query)
+    return "All good"
+
+
+@app.route('/questions', methods=['GET'])
+def questions():
+    data = dbFetch("SELECT * FROM answer WHERE question_id IS NOT NULL ORDER BY question_id")
+    questionsCount = data[-1]['question_id'] + 1
+    answersCount = len(data)
+
+    quiz = []
+    matrix = [[] for x in range(answersCount)]
+    prof = [None for x in range(answersCount)]
+
+    for i in range(questionsCount):
+        question = {}
+        question_answers = [x for x in data if x['question_id'] == i]
+        for answer in question_answers:
+            question[answer['answer_id']] = answer['text']
+        quiz.append(question)
+
+    for i in range(answersCount):
+        answer_id = data[i]["answer_id"]
+
+        matrix[answer_id] = [
+            data[i]["270302"],
+            data[i]["270304"],
+            data[i]["270305"],
+            data[i]["090301"],
+            data[i]["090303"],
+            data[i]["090304"],
+            data[i]["270303"],
+            data[i]["090302"],
+            data[i]["020302"],
+            data[i]["020303"]
+        ]
+
+        prof[answer_id] = data[i]["prof_id"]
+
+    result = {
+        "quiz": quiz,
+        "matrix": matrix,
+        "prof": prof
+    }
+
+    return json.JSONEncoder().encode(result)
+
+
+@app.route('/directions', methods=['GET'])
+def directions():
+    data = dbFetch("SELECT * FROM direction")
+
+    result = []
+    for i in data:
+        result.append({
+            "id": i["id"],
+            "title": i["title"],
+            "description": i["description"],
+            "link": i["link"]
+        })
+    return json.JSONEncoder().encode(result)
+
+
+@app.route('/professions', methods=['GET'])
+def professions():
+    data = dbFetch("SELECT * FROM profession")
+
+    result = []
+    for i in data:
+        result.append({
+            "id": i["id"],
+            "value": i["title"]
+        })
+    return json.JSONEncoder().encode(result)
+
+
+def dbFetch(query):
+    response = 'something wrong'
 
     try:
         connection = pymysql.connect(
@@ -23,13 +105,8 @@ def users():
 
         try:
             with connection.cursor() as cursor:
-                insert_query = "INSERT INTO `user` (first_name, last_name, email, math, rus, phys, inf, individual) VALUES " \
-                               "('%s', '%s', '%s', %i, %i, %i, %i, %i);" % (
-                                   req["firstname"], req["lastname"], req["email"],
-                                   req["math"], req["rus"], req["phys"], req["inf"],
-                                   req["individual"])
-                cursor.execute(insert_query)
-
+                cursor.execute(query)
+                response = cursor.fetchall()
             connection.commit()
 
         finally:
@@ -38,39 +115,4 @@ def users():
     except Exception as e:
         print(e)
 
-    return "All good"
-
-@app.route('/questions', methods=['GET'])
-def questions():
-    data = getJsonData('db.json')
-    quizStructure = data["quiz"]
-    questionsList = data["text"]
-    matrix = data["matrix"]
-    prof_coeffs = data["prof"]
-
-    result = {
-        "quiz": [],
-        "matrix": matrix,
-        "prof": prof_coeffs
-    }
-    for quizQuestion in quizStructure:
-        result["quiz"].append({})
-        for quizQuestionItem in quizQuestion:
-            result["quiz"][-1][quizQuestionItem] = questionsList[int(quizQuestionItem)]
-
-    return json.JSONEncoder().encode(result)
-
-
-@app.route('/directions', methods=['GET'])
-def directions():
-    return json.JSONEncoder().encode(getJsonData("directions.json"))
-
-
-@app.route('/professions', methods=['GET'])
-def professions():
-    return json.JSONEncoder().encode(getJsonData("professions.json"))
-
-
-def getJsonData(path):
-    with open(path, 'r', encoding='utf-8') as fh:
-        return json.load(fh)
+    return response
